cedbcaad8b8e2fb1e26626762b3cbd77a61ac27c
al1na-am1rova <li.amirova2000@mail.ru>
2023-09-14 21:43:30 +0300

    add struct

---
+++ Amirova_lab_1/Amirova_lab_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+struct oil_pipe
+{
+    string name;
+    float lenght;
+    int diameter;
+    bool reparied = false;
+};
+
+struct oil_pumping_station
+{
+    string name;
+    int number_of_guild;
+    int number_of_working_guild;
+    int effectiveness;
+
+};
+
+int main()
+{
+
+}
