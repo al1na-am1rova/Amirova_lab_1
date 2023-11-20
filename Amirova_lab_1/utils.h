70c9b0816e7ac70c650e6dd29ac5ba2fa376ba66
al1na-am1rova <li.amirova2000@mail.ru>
2023-11-20 12:11:04 +0300

    add topological sorting

--- Amirova_lab_1/utils.h
+++ Amirova_lab_1/utils.h
@@ -137,36 +137,46 @@ int is_pipe_in_system(int id, unordered_map<int, CSystem> systems) {
     return -1;
 }
 
-unordered_map<int, int> create_graph(unordered_map<int, CSystem>& systems) {
-    unordered_map<int, int> graph;
-    for (auto i : systems) graph.insert({ i.second.entrance_id, i.second.exit_id });
+unordered_map<int, vector<int>> create_graph(unordered_map<int, CSystem>& systems) {
+    unordered_map<int, vector<int>> graph;
+    for (auto i : systems) {
+        if (graph.find(i.second.entrance_id) == graph.end()) {
+            vector<int> current = { i.second.exit_id };
+            graph.insert({ i.second.entrance_id, current });
+        }
+        else {
+            graph.at(i.second.entrance_id).push_back(i.second.exit_id);
+        }
+    }
     return graph;
 }
 
-//void dfs(vector<vector<int>> graph, int v, vector<int>& visited, vector<int>& order) {
-//    visited[v] = 1;
-//    for (int i : graph.at(v)) if (!visited[i]) dfs(graph, i, visited, order);
-//    order.push_back(v);
-//    return;
-//}
+void dfs(unordered_map<int, vector<int>>& graph, int v, vector<int>& visited, vector<int>& order) {
+    visited.push_back(v);
+    if (graph.find(v) != graph.end()){
+        for (int i : graph.at(v)) if (find(visited.begin(), visited.end(), i) == visited.end()) {
+            dfs(graph, i, visited, order);
+        }
+    }
+    order.push_back(v);
+    return;
+}
 
 void sort_graph(unordered_map<int, CSystem> systems) {
-    vector<vector<int>> graph = create_graph(systems);
+    unordered_map<int, vector<int>> graph = create_graph(systems);
+    /*for (auto i : graph) cout << i.first << i.second[0] << "\t";*/
     vector<int> counter;
     for (auto i : systems) {
         if (find(counter.begin(), counter.end(), i.second.entrance_id) == counter.end()) counter.push_back(i.second.entrance_id);
         if (find(counter.begin(), counter.end(), i.second.exit_id) == counter.end()) counter.push_back(i.second.exit_id);
     }
- /*   for (auto i : graph) {
-        cout << i.first;
-        for (int j : i.second) cout << j << " ";
-        cout << endl;
-    }
-    cout << counter.size();*/
-   /* vector<int> visited(counter.size());
+    vector<int> visited;
     vector<int> order;
-    for (int v = 0; v < counter.size(); v++) if (!visited[v]) dfs(graph, v, visited, order);
+    counter.pop_back();
+        /*for (auto i : counter) cout << i << "\t";*/
+    for (int v :counter) if (find(visited.begin(), visited.end(), v) == visited.end()) dfs(graph, v, visited, order);
     reverse(order.begin(), order.end());
     cout << "Result" << endl;
-    for (int v : order) cout << v << endl;*/
+    for (int v : order) cout << v << '\t';
+    cout << endl;
 }
